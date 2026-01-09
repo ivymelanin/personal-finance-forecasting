69d78e77ea898885522a9bd6b19052fcba58964b
Lerato Khauta <leratokhauta20@gmail.com>
2026-01-09 14:47:56 +0200

    Add category management to finance app
    Introduces category persistence using a JSON file and session state. Users can now add new categories via the UI, which are saved and loaded automatically.

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ import os
 
 st.set_page_config(page_title="Finance Forecasting", page_icon="", layout="wide")
 
+category_file = "categories.json"
+
+if "categories" not in st.session_state:
+    st.session_state.categories = {
+        "Uncategorized": [], 
+        "New category": []
+    }
+if os.path.exists("categories.json"): 
+    with open(category_file, "r") as f:
+        st.session_state.categories = json.load(f)
+
+def save_categories():
+    with open(category_file, "w") as f:
+        json.dump(st.session_state.categories, f)
+    
 def load_transactions(file):
     try: 
         df = pd.read_csv(file)
@@ -29,7 +44,17 @@ def main():
             credits_df = df[df["Debit/Credit"] == "Credit"].copy()
 
             tab1, tab2 = st.tabs(["Expense (Debits)", "Payments (Credits)"])
-            with tab1: 
+            with tab1:
+                new_category = st.text_input("New Category Name")
+                add_button = st.button("Add category")
+
+                if add_button and new_category:
+                    if new_category not in st.session_state.categories:
+                        st.session_state.categories[new_category] = []
+                        save_categories()
+                        st.success(f"Added a new category:{new_category}")
+                        st.rerun()
+
                 st.write(debits_df)
 
             with tab2: 
